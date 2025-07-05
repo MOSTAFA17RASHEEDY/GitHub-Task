73e1724046aa32fafd3fe8e426c42cf3be9999c4
Alaa abd el hady zaky <alaa20-01773@student.eelu.edu.eg>
2025-07-05 11:59:16 +0300

    Add initial changes in AlaaAbdelhady branch

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
     Promise.all([promise1, promise2, promise3])
     .then(Response => {
         console.log(Response); 
-    })
+    })    
     .catch(error => {
         console.log("error all",error);
     });
